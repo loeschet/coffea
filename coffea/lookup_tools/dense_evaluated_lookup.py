15be01b1d42129a867f957d79932674677c854be
Christina Reissel [chreisse] <christina.reissel@cern.ch>
2019-07-09 16:45:02 +0200

    update btagging weights for cuda

--- coffea/lookup_tools/dense_evaluated_lookup.py
+++ coffea/lookup_tools/dense_evaluated_lookup.py
@@ -1,16 +1,24 @@
 from ..lookup_tools.lookup_base import lookup_base
 
 from ..util import numpy as np
+import numpy
 from ..util import awkward
 from ..util import numba
 
 from copy import deepcopy
 
+from ..util import USE_CUPY
+from .jme_standard_function import searchsorted_wrapped
+
+if USE_CUPY:
+    import cupy as np
+    import numpy
+
 
 # methods for dealing with b-tag SFs
 @numba.jit(forceobj=True)
 def numba_apply_1d(functions, variables):
-    out = np.empty(variables.shape)
+    out = numpy.empty(variables.shape)
     for i in range(functions.size):
         out[i] = functions[i](variables[i])
     return out
@@ -45,15 +53,15 @@ class dense_evaluated_lookup(lookup_base):
                             'str' in values.dtype.name or
                             'unicode' in values.dtype.name or
                             'bytes' in values.dtype.name)  # ....
-        if not isinstance(values, np.ndarray):
-            raise TypeError('values is not a numpy array, but %r' % type(values))
+        #if not isinstance(values, np.ndarray):
+        #    raise TypeError('values is not a numpy array, but %r' % type(values))
         if not vals_are_strings:
             raise Exception('Non-string values passed to dense_evaluated_lookup!')
         if feval_dim is None:
             raise Exception('Evaluation dimensions not specified in dense_evaluated_lookup')
-        funcs = np.zeros(shape=values.shape, dtype='O')
+        funcs = numpy.zeros(shape=values.shape, dtype='O')
         for i in range(values.size):
-            idx = np.unravel_index(i, shape=values.shape)
+            idx = numpy.unravel_index(i, shape=values.shape)
             funcs[idx] = numbaize(values[idx], ['x'])
         self._values = deepcopy(funcs)
         # TODO: support for multidimensional functions and functions with variables other than 'x'
@@ -67,13 +75,17 @@ class dense_evaluated_lookup(lookup_base):
             if type(arg) == awkward.JaggedArray:
                 raise Exception('JaggedArray in inputs')
         if self._dimension == 1:
-            indices.append(np.clip(np.searchsorted(self._axes, args[0], side='right') - 1, 0, self._values.shape[0] - 1))
+            indices.append(np.clip(searchsorted_wrapped(self._axes, args[0], side='right') - 1, 0, self._values.shape[0] - 1))
         else:
             for dim in range(self._dimension):
-                indices.append(np.clip(np.searchsorted(self._axes[dim], args[dim], side='right') - 1,
+                indices.append(np.clip(searchsorted_wrapped(self._axes[dim], args[dim], side='right') - 1,
                                        0, self._values.shape[len(self._axes) - dim - 1] - 1))
         indices.reverse()
-        return numba_apply_1d(self._values[tuple(indices)], args[self._feval_dim])
+        if USE_CUPY:
+            var = np.asnumpy(args[self._feval_dim])
+        else:
+            var = args[self._feval_dim]
+        return np.asarray(numba_apply_1d(self._values[tuple(indices)], var))
 
     def __repr__(self):
         myrepr = "{} dimensional histogram with axes:\n".format(self._dimension)
